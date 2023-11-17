554eff2d2865880f9348f24086e738dab4a5a7bc
Sumit Pandey <73009052+Sumit-Pandey-07@users.noreply.github.com>
2023-11-17 13:03:14 +0530

    Add files via upload

---
+++ src/components/GPTMovieSuggestions.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+
+import MovieList from "./MovieList";
+const GPTMovieSuggestions = () => {
+  const { movieResults, movieNames } = useSelector((store) => store?.gpt);
+
+  if (!movieNames) return null;
+
+  return (
+    <>
+      <div className=' z-40 p-4 absolute -mt-[0px] md:mt-[150px]'>
+        <div className=' text-white font-bold'>
+          {movieNames.map((movieName, index) => (
+            <MovieList
+              key={movieName}
+              title={movieName}
+              movies={movieResults[index]}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GPTMovieSuggestions;
